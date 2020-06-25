e6ec34186c6170e874a6a1e61d7d68450fdf47be
cristianaxion <49574617+cristianaxion@users.noreply.github.com>
2020-06-25 17:10:01 -0300

    Update peroo.sh

--- peroo.sh
+++ peroo.sh
@@ -9,7 +9,7 @@ resultado=$(expr $num1 \* $num2)
 echo el resultado es $resultado
 #
 resultado=$(expr $num1 + $num2)
-echo el resultado es $resultado
+echo el resultado es $rsultado
 #
 resultado=$(expr $num1 - $num2)
 echo el resultado es $resultado
@@ -20,7 +20,5 @@ if [$num1 -gt $num2];
 then
 echo "primero numero: " $num1 " y es mayor al segundo numero: "$num2
 else
-echo "segundo numero: " $num2 " y es mayor al primer numero: "$num1
 
-fi
 
